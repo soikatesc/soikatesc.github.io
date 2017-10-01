fe648aa9f3f22f405862e8fdef8291d7a67f107b
Md Rahaman <soikatesc@gmail.com>
2017-10-01 02:04:29 -0500

    added certification section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,4 +6,6 @@ $( document ).ready(function(){
       closeOnClick: true   // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
     $('.parallax').parallax();
+    $('ul.tabs').tabs();
+    $('.materialboxed').materialbox();
 })
